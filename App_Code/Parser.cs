a420f310fdb2dfde3ba3945f1fe8b766753df61a
lolipopper <njruntuwene@gmail.com>
2016-04-24 21:39:13 +0900

    add parser

---
+++ App_Code/Parser.cs
@@ -0,0 +1,14 @@
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+public static class Parser
+{
+    public string[] parse(string s)
+    {
+        char[] delimiterChars = {';',' '};
+        return text.Split(delimiterChars);
+    }
+}
